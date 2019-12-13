fba7213590a98311d6a18ec2f449c775666d0f5e
zhanglei <zhanglei1@chelun.com>
2019-12-13 14:52:38 +0800

    用例测试

--- test.php
+++ test.php
@@ -2,20 +2,20 @@
 $file = "cache.log";
 $handle= fopen($file,"r");
 $data = "";
- while (!feof ($handle)) {
- $buffer  = fgets($handle, 50000);
+while (!feof ($handle)) {
+    $buffer  = fgets($handle, 50000);
 
-     $buffer = trim($buffer);
-     if($buffer){
-     if(!$data)
-     {
-        $data = $buffer;
-     }else{
-     if($buffer != $data)
-     {
-     var_dump(false);
-     }else{
-     }
-     }
-     }
- }
+    $buffer = trim($buffer);
+    if($buffer){
+        if(!$data)
+        {
+            $data = $buffer;
+        }else{
+            if($buffer != $data)
+            {
+                var_dump(false);
+            }else{
+            }
+        }
+    }
+}
--- test2.php
+++ test2.php
@@ -1,21 +1,21 @@
 <?php
-$file = "cache.log";
+$file = "cache2.log";
 $handle= fopen($file,"r");
 $data = "";
- while (!feof ($handle)) {
- $buffer  = fgets($handle, 50000);
+while (!feof ($handle)) {
+    $buffer  = fgets($handle, 50000);
 
-     $buffer = trim($buffer);
-     if($buffer){
-     if(!$data)
-     {
-        $data = $buffer;
-     }else{
-     if($buffer != $data)
-     {
-     var_dump(false);
-     }else{
-     }
-     }
-     }
- }
+    $buffer = trim($buffer);
+    if($buffer){
+        if(!$data)
+        {
+            $data = $buffer;
+        }else{
+            if($buffer != $data)
+            {
+                var_dump(false);
+            }else{
+            }
+        }
+    }
+}
